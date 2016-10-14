a78901a78a2a57283c7339417c8e9b06fde04052
Siloow <sil.de.graaf96@gmail.com>
2016-10-14 15:44:48 +0200

    Summerized up to Texas ranges

--- haskell_practice.hs
+++ haskell_practice.hs
@@ -1,7 +1,151 @@
--- Chapter 1
--- Quick test
+-- For future self:
+-- This is a summary of Learn you a Haskell for a great good.
+-- You went through every chapter and wrote down the most important stuff.
+-- If you encounter "-- >" it means you can test it out in WinGHci or something.
+-- If you encounter "-- =" it means the result of the "-- >" above.
+-- [Some kind of title] means a new paragraph or chapter.
+-- Each chapter can be uncommeneted seperately so that the examples do interfere with eachother.
+-- Have fun
 
 
+-- Dictionary
+-- Statement : defines an action, consists of expressions (What code does)
+-- Expression : A collection of symbols that jointly express a quantity. It evaluates to a value. (What code is)
+-- 'Let' keyword : Used to define a name (and more..)
+
+-- [Chapter 1]
+-- [Calling functions]
+
+-- (*) is called an infix function.
+-- With prefix functions the name comes first, then a space then its paramaters.
+-- Function application hsa the highest precedence of all the operations in Haskell.
+
+-- By using backticks, we can call a prefix function as an infix function.
+-- > 92 `div` 10
+-- This is the same as calling a function like this in an imperative language : bar(bar(3)) 
+
+-- [Baby's first functions]
+doubleMe x = x + x
+doubleUs x y = x * 2 + y * 2
+
+-- > doubleUs 4 9
+-- = 26
+
+-- Functions in Haskell don't have to be defined in any particular order, so it doesn't matter which function comes first in a file.
+-- Functions can't begin with a capital letter
+
+-- The following is not a 'function' because it doens't take any paramaters. It is a 'definition' or a 'name'
+myNameIsSil = "I love cheese"
+
+-- Functions can also call eachother.
+doubleUs' x y = doubleMe x + doubleMe y
+
+-- An if in Haskell is an expression that must return a value, and not a statement.
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+
+-- The (') denotes either a strict version of a function (i.e. one that isn't lazy), or a slightly modified version.
+-- of a function or a variable with a similar name.
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+-- [An intro to lists]
+
+-- Lists in Haskell are homogeneous data structures, which means they store several elements of the same type.
+-- This means we can't have a list of both integers and strings.
+lostNumbers = [3,5,3,5,3,2,7567,567567]
+-- > lostNumbers
+-- = [3,5,3,5,3,2,7567,567567]
+
+-- [Concatenation]
+-- The concatenation operator : (++)
+-- let concatList = [1,2,4,5] ++ [9,10,11,12]
+-- > concatList
+-- = [1,2,4,5,9,10,11,12]
+
+-- in Haskell, string are lists of characters. Because of this, we can use list functions on strings.
+
+-- The (++) operator goes through the whole list, so try to avoid using it on big lists.
+-- However, adding something to the beginning of a list is a instantaneous operation.
+-- This is done with the (:) operator, called the 'cons' operator.
+-- The first argument to the cons operator always needs to be a single item of the same type of the list's items.
+-- > 5:[1,2,3,4]
+-- = [5,1,2,3,4]
+
+-- [1,2,3] is syntactic sugar for 1:2:3:[]
+
+-- [Accessing list elements]
+-- The operator : (!!)
+-- > [1,2,3,4,5] !! 2
+-- = 3
+
+-- [Lists inside lists]
+b = [[1,2,3,4],[5,4,3,2,1],[1,2,3]]
+-- > b
+-- = [[1,2,3,4],[5,4,3,2,1],[1,2,3]]
+-- > b !! 2
+-- = [1,2,3]
+
+-- [Comparing lists]
+-- Lists can be compared if the items they contain can be compared. When compared with boolean operators,
+-- they are compared in lexicographical order. This means the two list heads are compared, and if they're equal,
+-- the second elements are compared, and so on.
+isL1biggerThanL2 = [3,2,1] > [2,1,0]
+-- > isL1biggerThanL2
+-- = True
+isL1biggerThanL2' = [1,2,1] > [2,1,0]
+-- > let isL1biggerThanL2'
+-- = False
+
+-- [More list operators]
+-- head : first element
+-- > head [5,4,3,2,1]
+-- = 5
+-- tail : all but first
+-- > tail [5,4,3,2,1]
+-- = [4,3,2,1]
+-- last : last element
+-- > last [5,4,3,2,1]
+-- = 1
+-- init : all but last
+-- > init [5,4,3,2,1]
+-- = [5,4,3,2]
+-- length
+-- > length [1,2,3,4]
+-- = 4
+-- null : checks if a list is empty
+-- > null [1,2,3]
+-- = False 
+-- reverse
+-- > reverse [5,4,3,2,1]
+-- = [1,2,3,4,5]
+-- take : takes a number and a list and extracts the specified number
+-- If we try to take more elements than there are in the list, Haskell just returns the entire list. We if
+-- take 0 elements, we get an empty list.
+-- > take 3 [5,4,3,2,1]
+-- = [5,4,3]
+-- drop : similar as take but it drops the specified number from the beginning of a list
+-- > drop 4 [5,4,3,6,2,5]
+-- = [2,5]
+
+-- [Texas ranges]
+-- Ranges are used to make lists composed of elements that can be enumerated.
+-- > [1..20]
+-- = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+-- To define a step, separate the first two elements with a comma and specifiy the upper limit
+-- > [1,3..6]
+-- = [1,3,5]
+-- > [3,6..20]
+-- = [3,6,9,12,15,18]
+
+-- To make a list in desc order type:
+-- > [20, 19..1]
+
+-- You can make infinite lists. A way to use them:
+-- > take 24 [13,26..]
+-- = [13,26,39,52..321]
+
+-- A few functions that can be used to produce long or infinite lists
 
 
 -- Chapter 12 : Monoids
