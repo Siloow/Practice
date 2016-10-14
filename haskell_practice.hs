791ae3224586fa9fa7c2685584be4d6b45771501
Siloow <sil.de.graaf96@gmail.com>
2016-10-15 00:56:16 +0200

    Summerized up to Chapter 2

--- haskell_practice.hs
+++ haskell_practice.hs
@@ -12,8 +12,9 @@
 -- Statement : defines an action, consists of expressions (What code does)
 -- Expression : A collection of symbols that jointly express a quantity. It evaluates to a value. (What code is)
 -- 'Let' keyword : Used to define a name (and more..)
+-- Predicate : condition
 
--- [Chapter 1]
+-- [Chapter 1 : Introduction]
 -- [Calling functions]
 
 -- (*) is called an infix function.
@@ -144,11 +145,96 @@ isL1biggerThanL2' = [1,2,1] > [2,1,0]
 -- You can make infinite lists. A way to use them:
 -- > take 24 [13,26..]
 -- = [13,26,39,52..321]
+-- Because Haskell is lazy, it won't try to evaluate the entire list immediately.
+-- Instead, it will wait to see which elements you need to get from the infinite list.
 
 -- A few functions that can be used to produce long or infinite lists
+-- cycle : takes a list and replicates its elements indefinitely to form an infinite list.
+-- > take 10 (cycle [1,2,3])
+-- = [1,2,3,1,2,3,1,2,3]
+-- repeat : takes an element and produces an infinite list of just that element. Same as cycle but with one element
+-- > take 5 (repeat 5)
+-- = [5,5,5,5,5]
+-- replicate : is an easier way to create a list composed of a single item. It is like take 5 (repeat 5) with the take part built in.
+-- > replicate 3 10
+-- = [10,10,10]
+
+-- [I'm a list comprehension]
+-- List comprehensions are a way to filter, transform, and combine lists. 
+-- > [x*2 | x <- [1..10]]
+-- = [2,4,6,8,10,12,14,16,18,20]
+-- In this list comprehension we say that we draw our elements from the list [1..10]
+-- [x <- [1..10]] means that x takes on the value of each element that is drawn frow [1..10]
+-- In other words, we bind each element fom [1..10] to x. The part before the (|) is the output of the list comprehension.
+
+-- It is also possible to add a predicate to the list comprehension
+-- > [x*2 | x <- [1..10], x*2 >= 12]
+-- = [12,14,16,18,20]
+
+-- Another example that replaces every odd number < 10 with "Kaas" and > 10 with "Bier"
+kaasBier xs = [ if x < 10 then "Kaas" else "Bier" | x <- xs, odd x]
+-- > kaasBier [7..13]
+-- = ["Kaas", "Kaas", "Bier", "Bier"]
+-- You can include as many predicates as you want. Seperate them with commas.
+-- It is also possible to draw values from multiple lists.
+multiValueListResult = [x+y | x <- [1,2,3], y <- [10,100,1000]]
+-- > multiValueListResult
+-- = [11,101,1001,12,102,1002,13,103,1003]
+-- x is draw from [1,2,3] and y is drawn from [10,100,1000]. While x is 1, y takes on every value from [10,100,1000]
+-- and adds 1 to every element that y becomes.
+
+-- Another example 
+length' xs = sum [1 | _ <- xs]
+-- This function will replace every element in a list with 1, and then sums them all up with 'sum' that yield the length of the list.
+-- (_) is just a temporary variable to store items as we draw them. We don't care what type the element is.
+
+-- Another example
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+-- This will only include items in a new list that are in ['A'..'Z'] thus removes all lowercase
+-- > removeNonUppercase "KAASisBIER"
+-- = "KAASBIER"
+
+-- It is also possible to create nested list comprehensions.
+listCeption xxs = [ [ x | x <- xs, even x ] | xs <- xxs]
+-- > listCeption [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]] 
+-- = [[2,2,4],[2,4,6,8],[2,4,2,6,2,6]]
+-- Here the output of the outer list comprehension is another list comprehension. 
+
+-- [Tuples]
+-- Tuples are used to store several heterogeneous elements as a single value.
+-- Tuples have a fixed size.
+-- The size of a tuple is part of its type.
+-- > (1, 3)
+-- = (1, 3)
+
+-- A tuple of size two (pair) and a tuple of size three (triple) are treated as two distrinct types, which means a list can't be
+-- composed of both pairs and triples. A tuple (1, "Kaas") is a different type than ("Kaas", 1).
+
+-- Some functions that operate on tuples.
+-- fst : takes a pair and returns its first component
+-- > fst (8, 11)
+-- = 8
+-- snd : takes a pair and return its second component
+-- > snd (8, 11)
+-- = 11
+-- These functions only work on pairs.
+
+-- Another example
+-- zip : takes two lists, then "zips" them together into one list by matching elements into pairs. 
+-- > zip [1,2,3,4,5] [5,5,5,5,5]
+-- = [(1,5),(2,5),(3,5),(4,5),(5,5)]
+-- Zip can take two lists that contain elements of different types
+-- > zip [1..5] ["one", "two", "three", "four", "five"]
+-- = [(1,"one"),(2,"two"),(3,"three"),(4,"four"),(5,"five")]
+-- When de lengths of the lists don't match : 
+-- > zip [5,3,2,6,2,7,2,5,4,6,6] ["im","a","turtle"]  
+-- = [(5,"im"),(3,"a"),(2,"turtle")]
+
+-- [Chapter 2 : Types]
 
 
--- Chapter 12 : Monoids
+
+-- [Chapter 12 : Monoids]
 
 -- List type as applicative functor
 -- <*> takes every function out of the list that is its left parameter and applies it to every value in that list that is on the right.
